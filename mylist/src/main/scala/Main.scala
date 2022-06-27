e969d78aaa78d2172f34b9a5d10d04a643261c43
LeraIord <lera.iord8@gmail.com>
2022-06-27 19:57:23 +0300

    list

--- mylist/src/main/scala/Main.scala
+++ mylist/src/main/scala/Main.scala
@@ -1,6 +1,5 @@
 import scala.annotation.tailrec
 import scala.collection.mutable
-
 import math.Fractional.Implicits.infixFractionalOps
 import math.Integral.Implicits.infixIntegralOps
 import math.Numeric.Implicits.infixNumericOps
@@ -9,7 +8,28 @@ enum List[+A]:
   case Nil
   case Cons(hd: A, tl: List[A])
 
+  def tail: List[A] = this match{
+    case Nil => Nil
+    case Cons(hd, tl) => tl
+  }
+
+  def tryHead: Option[A] = this match {
+    case Nil => None
+    case Cons(hd,tl)=> Some(hd)
+  }
+
+  def map[B](f: (A) => B): List[B] = {
+    def go(xs: List[A], acc: List[B]): List[B] = {
+      xs match {
+        case List.Nil => acc
+        case List.Cons(xh, xt) => List.Cons(f(xh), go(xt, acc))
+      }
+    }
+    go(this,List.Nil)
+  }
+
   def foldLeft[B](x: B)(f: (B, A) => B): B = {
+    @tailrec
     def inner(xs: List[A], acc: B): B = xs match {
       case Nil          => acc
       case Cons(xh, xt) => inner(xt, f(acc, xh))
@@ -27,7 +47,7 @@ enum List[+A]:
   def isEmpty: Boolean =
     this match {
       case Nil        => true
-      case Cons(_,_ ) => false
+      case Cons(_, _) => false
     }
 
   override def toString: String = {
@@ -52,10 +72,25 @@ object List:
       Cons(x, acc)
     }
 
-def sum(l: List[Int]): Int = l.foldLeft(0)(_ + _)
+def sum_old(xs: List[Int]): Int = xs.foldLeft(0)(_ + _)  
 
-def dotProduct(u: List[Int], v: List[Int]): Int =
-  zipWith(u, v, (x, y) => x * y).foldLeft(0)(_ + _)
+def sum(xs: List[Int]): Int = xs match {
+  case List.Nil => 0 
+  case List.Cons(xh,xt) => xh + sum(xt)
+}
+
+def dotProduct_old(u: List[Int], v: List[Int]): Int =
+  zipWith(u, v, (x, y) => x * y).foldLeft(0)(_ + _) 
+
+def dotProduct(xs: List[Int], ys: List[Int]): Int = {
+  def go(xs: List[Int], ys: List[Int], acc: Int): Int = (xs, ys) match {
+    case (List.Cons(xh , xt), List.Cons(yh , yt)) => go(xt , yt, acc + xh * yh)
+    case (List.Nil, _) => acc
+    case (_, List.Nil) => acc
+  }
+
+  go(xs, ys, 0)
+}
 
 def zipWith[A, B, C](xs: List[A], ys: List[B], f: (A, B) => C): List[C] =
   (xs, ys) match {
@@ -64,8 +99,31 @@ def zipWith[A, B, C](xs: List[A], ys: List[B], f: (A, B) => C): List[C] =
       List.Cons(f(xh, yh), zipWith(xt, yt, f))
   }
 
+def map[A](list: List[A], f: (A) => A): 
+  List[A] = {
+    def go(xs: List[A], acc: List[A]): List[A] = {
+      xs match {
+        case List.Nil => acc
+        case List.Cons(xh, xt) => List.Cons(f(xh), go(xt, acc))
+      }
+    }
+    go(list,List.Nil)
+  }
+
+//Намагалася, але виникає помилка. 
+// def transpose[T](l: List[List[T]]): List[List[Option[T]]] =
+//    l map {_.tryHead} match {
+//        case List.Nil => List.Nil
+//        case List.Cons(head,tail) => List.Cons(head, transpose(l.tail))
+//    }
+
+
+
+
 @main def main(): Unit = {
 
 println("Hello World!")
 
 }
+
+
--- mylist/src/test/scala/MySuite.scala
+++ mylist/src/test/scala/MySuite.scala
@@ -2,76 +2,124 @@ import List.*
 class MySuite extends munit.FunSuite {
 
 
-test("sum test1") {
-  val obtained = 10
-  val expected = sum(List(5,5))
-  assertEquals(obtained, expected)
+  test("sum test1") {
+    val obtained = 10
+    val expected = sum(List(5,5))
+    assertEquals(obtained, expected)
   }
 
-test("sum test2") {
-  val obtained = 20
-  val expected = sum(List(5,5,5,5))
-  assertEquals(obtained, expected)
+  test("sum test2") {
+    val obtained = 20
+    val expected = sum(List(5,5,5,5))
+    assertEquals(obtained, expected)
   }
 
-test("sum test3") {
-  val obtained = 15
-  val expected = sum(List(0,5,5,5))
-  assertEquals(obtained, expected)
+  test("sum test3") {
+    val obtained = 15
+    val expected = sum(List(0,5,5,5))
+    assertEquals(obtained, expected)
   }
 
-test("sum test4") {
-  val obtained = 0
-  val expected = sum(List(0,0,0,0))
-  assertEquals(obtained, expected)
+  test("sum test4") {
+    val obtained = 0
+    val expected = sum(List())
+    assertEquals(obtained, expected)
+  }
+  test("sum_old test1") {
+    val obtained = 10
+    val expected = sum_old(List(5,5))
+    assertEquals(obtained, expected)
+  }  
+
+  test("sum_old test2") {
+    val obtained = 0
+    val expected = sum_old(List(1,-1))
+    assertEquals(obtained, expected)
+  }   
+
+  test("sum_old test3") {
+    val obtained = 0
+    val expected = sum_old(List())
+    assertEquals(obtained, expected)
+  }    
+
+  test("sum_old test4") {
+    val obtained = 10
+    val expected = sum_old(List(1,2,3,4))
+    assertEquals(obtained, expected)
+  }    
+
+
+  test("foldLeft test1") {
+    val obtained = 10
+    val expected = List(5,5).foldLeft(0)(_+_)
+    assertEquals(obtained, expected)
   }
 
-test("foldLeft test1") {
-  val obtained = 10
-  val expected = List(5,5).foldLeft(0)(_+_)
-  assertEquals(obtained, expected)
-  }
-
-test("foldLeft test2") {
-  val obtained = 30
-  val expected = List(1,2,5,3).foldLeft(1)(_*_)
-  assertEquals(obtained, expected)
+  test("foldLeft test2") {
+    val obtained = 30
+    val expected = List(1,2,5,3).foldLeft(1)(_*_)
+    assertEquals(obtained, expected)
   }
   
-test("foldLeft test3") {
-  val obtained = 0
-  val expected = List(1,2,5,0).foldLeft(1)(_*_)
-  assertEquals(obtained, expected)
+  test("foldLeft test3") {
+    val obtained = 0
+    val expected = List(1,2,5,0).foldLeft(1)(_*_)
+    assertEquals(obtained, expected)
   }
   
-test("foldLeft test4") {
-  val obtained = 1
-  val expected = List(1,0).foldLeft(0)(_+_)
-  assertEquals(obtained, expected)
+  test("foldLeft test4") {
+    val obtained = 1
+    val expected = List(1,0).foldLeft(0)(_+_)
+    assertEquals(obtained, expected)
   }
 
   test("dotProduct test1") {
-  val obtained = 32
-  val expected = dotProduct(List(1,2,5,3),List(1,2,3,4))
-  assertEquals(obtained, expected)
+    val obtained = 32
+    val expected = dotProduct(List(1,2,5,3),List(1,2,3,4))
+    assertEquals(obtained, expected)
   }
 
   test("dotProduct test2") {
-  val obtained = 14
-  val expected = dotProduct(List(1,2,3),List(1,2,3))
-  assertEquals(obtained, expected)
+    val obtained = 14
+    val expected = dotProduct(List(1,2,3),List(1,2,3))
+    assertEquals(obtained, expected)
   }
 
   test("dotProduct test3") {
-  val obtained = 32
-  val expected = dotProduct(List(1,2),List(12,10))
-  assertEquals(obtained, expected)
+    val obtained = 32
+    val expected = dotProduct(List(1,2),List(12,10))
+    assertEquals(obtained, expected)
   }
 
   test("dotProduct test4") {
-  val obtained =0
-  val expected = dotProduct(List(0),List(0))
-  assertEquals(obtained, expected)
+    val obtained =0
+    val expected = dotProduct(List(),List())
+    assertEquals(obtained, expected)
+  }
+
+  test("dotProduct_old test1") {
+    val obtained = 31
+    val expected = dotProduct_old(List(0,2,5,3),List(1,2,3,4))
+    assertEquals(obtained, expected)
+  }
+
+  test("dotProduct_old test2") {
+    val obtained = 0
+    val expected = dotProduct_old(List(),List())
+    assertEquals(obtained, expected)
+  }
+
+  test("dotProduct_old test3") {
+    val obtained = 4
+    val expected = dotProduct_old(List(-2,3),List(1,2))
+    assertEquals(obtained, expected)
+  }
+
+  test("dotProduct_old test4") {
+    val obtained = 0
+    val expected = dotProduct_old(List(-2,3),List(0,0))
+    assertEquals(obtained, expected)
   }
 
   test("zipWith test1"){
@@ -80,8 +128,8 @@ test("foldLeft test4") {
     assertEquals(obtained, expected)
   }
   test("zipWith test2"){
-    val obtained = List(5,7,9)
-    val expected = (zipWith(List(1, 2, 3), List(4, 5, 6),(_ + _)))
+    val obtained = List(0,0,0)
+    val expected = (zipWith(List(0, 0, 0), List(0, 0, 0),(_ + _)))
     assertEquals(obtained, expected)
   }
   test("zipWith test3"){
@@ -103,26 +151,26 @@ test("foldLeft test4") {
   }
 
   test("IsEmpty test1") {
-  val obtained = true 
-  val expected =  List().isEmpty
-  assertEquals(obtained, expected)
-  }
+    val obtained = true 
+    val expected =  List().isEmpty
+    assertEquals(obtained, expected)
+ }
 
   test("IsEmpty test2") {
-  val obtained = false 
-  val expected =  List('a').isEmpty
-  assertEquals(obtained, expected)
+    val obtained = false 
+    val expected =  List('a').isEmpty
+    assertEquals(obtained, expected)
   }
 
   test("foldRight test1") {
-  val obtained = 10
-  val expected = List(5,5).foldRight(0)(_+_)
-  assertEquals(obtained, expected)
+    val obtained = 10
+    val expected = List(5,5).foldRight(0)(_+_)
+    assertEquals(obtained, expected)
   }
   test("foldRight test2") {
-  val obtained = 1
-  val expected = List(6,5).foldRight(0)(_-_)
-  assertEquals(obtained, expected)
+    val obtained = 1
+    val expected = List(6,5).foldRight(0)(_-_)
+    assertEquals(obtained, expected)
   }
 
   test("toString test1"){
